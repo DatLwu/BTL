b85fcac3f119526780e8cb9ad0e59d3d885703cf
DatLwu <datluu351@gmail.com>
2023-12-24 14:32:34 +0700

    2nd commit

--- Cafe/Controllers/HoaDonController.cs
+++ Cafe/Controllers/HoaDonController.cs
@@ -240,19 +240,19 @@ namespace Cafe.Controllers
                 return File(stream,"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",fileName);
             }
         }  
-        public async Task<IActionResult> Index(int? page, int? PageSize){
-            ViewBag.PageSize= new List<SelectListItem>(){
-                new SelectListItem() { Value="3", Text="3"},
-                new SelectListItem() { Value="5", Text="5"},
-                new SelectListItem() { Value="10", Text="10"},
-                new SelectListItem() { Value="15", Text="15"},
-                new SelectListItem() { Value="25", Text="25"},
-                new SelectListItem() { Value="50", Text="50"},
-            };
-            int pagesize = (PageSize ?? 3);
-            ViewBag.psize= pagesize;
-            var model = _context.KhachHang.ToList().ToPagedList(page ?? 1, pagesize);
-            return View(model);
-        }
+        // public async Task<IActionResult> Index(int? page, int? PageSize){
+        //     ViewBag.PageSize= new List<SelectListItem>(){
+        //         new SelectListItem() { Value="3", Text="3"},
+        //         new SelectListItem() { Value="5", Text="5"},
+        //         new SelectListItem() { Value="10", Text="10"},
+        //         new SelectListItem() { Value="15", Text="15"},
+        //         new SelectListItem() { Value="25", Text="25"},
+        //         new SelectListItem() { Value="50", Text="50"},
+        //     };
+        //     int pagesize = (PageSize ?? 3);
+        //     ViewBag.psize= pagesize;
+        //     var model = _context.HoaDon.ToList().ToPagedList(page ?? 1, pagesize);
+        //     return View(model);
+        // }
     }
 }
